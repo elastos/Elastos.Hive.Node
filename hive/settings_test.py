99f85ebc564fa4526e976efa4d3e9b5a3f90f12e
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-08-20 11:29:59 -0400

    Fixing an issue on mac

--- hive/settings_test.py
+++ hive/settings_test.py
@@ -4,7 +4,7 @@ TESTING = True
 
 DID_SIDECHAIN_URL = config('DID_SIDECHAIN_URL', default="http://api.elastos.io:21606", cast=str)
 
-MONGO_HOST = config('MONGO_HOST', default="172.17.0.1", cast=str)
+MONGO_HOST = config('MONGO_HOST', default="localhost", cast=str)
 MONGO_PORT = config('MONGO_PORT', default=27020, cast=int)
 MONGO_DBNAME = "hivedb_test"
 

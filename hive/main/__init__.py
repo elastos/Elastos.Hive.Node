db4fdb94ee5fb5adc00302a06cd1f36fd6d46283
patternmap <patternmap@163.com>
2020-09-14 15:45:27 +0800

    1. #4bqb0d Change respose format and add interceptor for server exception.

--- hive/main/__init__.py
+++ hive/main/__init__.py
@@ -1,4 +1,4 @@
-from . import view, view_db, view_file, view_scripting
+from . import view, view_db, view_file, view_scripting, interceptor
 
 
 def init_app(app):
@@ -6,3 +6,4 @@ def init_app(app):
     view_db.init_app(app)
     view_file.init_app(app)
     view_scripting.init_app(app)
+    interceptor.init_app(app)

15fbfa300d95e47d44adbc6d862faf37e27e84d4
patternmap <patternmap@163.com>
2020-10-14 16:48:33 +0800

    CU-4qtaqx add init log title of hive node.

--- hive/main/__init__.py
+++ hive/main/__init__.py
@@ -1,7 +1,19 @@
 from . import view, view_db, view_file, view_scripting, interceptor
+import logging
+
+
+logger = logging.getLogger()
+logger.level = logging.INFO
 
 
 def init_app(app):
+    logging.getLogger("Hive").info("##############################")
+    logging.getLogger("Hive").info("HIVE BACK-END IS STARTING")
+    logging.getLogger("Hive").info("##############################")
+
+    # print("##############################")
+    # print("HIVE BACK-END IS STARTING")
+    # print("##############################")
     view.init_app(app)
     view_db.init_app(app)
     view_file.init_app(app)

7dbe3fe35f9ea84da1dec8e29039573569db3ce0
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-10-28 22:32:06 -0400

    Added support for target_app_did and fixed a bug where $ could not be used from scripting while registering scripts

--- hive/main/hive_scripting.py
+++ hive/main/hive_scripting.py
@@ -11,12 +11,13 @@ from hive.util.constants import SCRIPTING_SCRIPT_COLLECTION, \
     SCRIPTING_EXECUTABLE_TYPE_INSERT, SCRIPTING_CONDITION_TYPE_QUERY_HAS_RESULTS, SCRIPTING_EXECUTABLE_TYPE_AGGREGATED, \
     SCRIPTING_EXECUTABLE_TYPE_UPDATE, SCRIPTING_EXECUTABLE_TYPE_DELETE, SCRIPTING_EXECUTABLE_TYPE_FILE_DOWNLOAD, \
     SCRIPTING_EXECUTABLE_TYPE_FILE_PROPERTIES, SCRIPTING_EXECUTABLE_TYPE_FILE_HASH, SCRIPTING_EXECUTABLE_DOWNLOADABLE, \
-    SCRIPTING_EXECUTABLE_TYPE_FILE_UPLOAD
+    SCRIPTING_EXECUTABLE_TYPE_FILE_UPLOAD, SCRIPTING_EXECUTABLE_CALLER_DID, SCRIPTING_EXECUTABLE_PARAMS
 from hive.util.did_info import get_collection
 from hive.util.did_mongo_db_resource import gene_mongo_db_name, query_update_one, populate_options_update_one
 from hive.util.did_scripting import check_json_param, run_executable_find, run_condition, run_executable_insert, \
     run_executable_update, run_executable_delete, run_executable_file_download, run_executable_file_properties, \
-    run_executable_file_hash, run_executable_file_upload
+    run_executable_file_hash, run_executable_file_upload, massage_keys_with_dollar_signs, \
+    unmassage_keys_with_dollar_signs
 from hive.util.server_response import ServerResponse
 
 
@@ -96,7 +97,7 @@ class HiveScripting:
         executable_body = executable.get('body')
         if executable_type == SCRIPTING_EXECUTABLE_TYPE_AGGREGATED:
             if not isinstance(executable_body, list):
-                return f"Invalid parameters passed for executble type '{executable_type}'"
+                return f"Invalid parameters passed for executable type '{executable_type}'"
             if len(executable_body) == 1:
                 return self.__executable_validation(executable_body[0])
             else:
@@ -112,17 +113,8 @@ class HiveScripting:
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_DELETE:
             return check_json_param(executable_body, f"{executable.get('name')}", args=["collection", "filter"])
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_UPDATE:
-            err_message = check_json_param(executable_body, f"{executable.get('name')}",
+            return check_json_param(executable_body, f"{executable.get('name')}",
                                            args=["collection", "filter", "update"])
-            if err_message:
-                return err_message
-            # We need to make sure to put extra quotation around "$set" or mongo update is going to fail because
-            # it's going to try to validate it
-            update_set = executable_body.get('update').get("$set", None)
-            if update_set:
-                executable_body["update"]["'$set'"] = update_set
-                executable_body['update'].pop("$set", None)
-            return None
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_FILE_DOWNLOAD:
             executable_name = executable.get('name')
             # We need to make sure that the script's name is not "_download" as it's a reserved field
@@ -135,26 +127,28 @@ class HiveScripting:
         else:
             return f"invalid executable type '{executable_type}'"
 
-    def __condition_execution(self, did, app_id, target_did, condition, params):
+    def __condition_execution(self, did, app_did, target_did, target_app_did, condition, params):
         condition_type = condition.get('type')
         condition_body = condition.get('body')
         if condition_type in [SCRIPTING_CONDITION_TYPE_AND, SCRIPTING_CONDITION_TYPE_OR]:
             if len(condition_body) == 1:
-                return self.__condition_execution(did, app_id, target_did, condition_body[0], params)
+                return self.__condition_execution(did, app_did, target_did, target_app_did, condition_body[0], params)
             new_condition = {
                 "type": condition_type,
                 "body": condition_body[1:]
             }
             if condition_type == SCRIPTING_CONDITION_TYPE_AND:
-                return self.__condition_execution(did, app_id, target_did, condition_body[0], params) and \
-                       self.__condition_execution(did, app_id, target_did, new_condition, params)
+                return self.__condition_execution(did, app_did, target_did, target_app_did, condition_body[0],
+                                                  params) and \
+                       self.__condition_execution(did, app_did, target_did, target_app_did, new_condition, params)
             elif condition_type == SCRIPTING_CONDITION_TYPE_OR:
-                return self.__condition_execution(did, app_id, target_did, condition_body[0], params) or \
-                       self.__condition_execution(did, app_id, target_did, new_condition, params)
+                return self.__condition_execution(did, app_did, target_did, target_app_did, condition_body[0],
+                                                  params) or \
+                       self.__condition_execution(did, app_did, target_did, target_app_did, new_condition, params)
         else:
-            return run_condition(did, app_id, target_did, condition_body, params)
+            return run_condition(did, app_did, target_did, target_app_did, condition_body, params)
 
-    def __executable_execution(self, did, app_id, target_did, executable, params, output={}, output_key=None, capture_output=False):
+    def __executable_execution(self, did, app_did, target_did, target_app_did, executable, params, output={}, output_key=None, capture_output=False):
         executable_type = executable.get('type')
         executable_body = executable.get('body')
         if not output_key:
@@ -166,27 +160,27 @@ class HiveScripting:
         if executable_type == SCRIPTING_EXECUTABLE_TYPE_AGGREGATED:
             err_message = None
             for i, e in enumerate(executable_body):
-                self.__executable_execution(did, app_id, target_did, e, params, output, e.get('name'), e.get('output', False))
+                self.__executable_execution(did, app_did, target_did, target_app_did, e, params, output, e.get('name'), e.get('output', False))
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_FIND:
-            data, err_message = run_executable_find(did, app_id, target_did, executable_body, params)
+            data, err_message = run_executable_find(did, app_did, target_did, target_app_did, executable_body, params)
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_INSERT:
-            data, err_message = run_executable_insert(did, app_id, target_did, executable_body, params)
+            data, err_message = run_executable_insert(did, app_did, target_did, target_app_did, executable_body, params)
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_UPDATE:
-            data, err_message = run_executable_update(did, app_id, target_did, executable_body, params)
+            data, err_message = run_executable_update(did, app_did, target_did, target_app_did, executable_body, params)
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_DELETE:
-            data, err_message = run_executable_delete(did, app_id, target_did, executable_body, params)
+            data, err_message = run_executable_delete(did, app_did, target_did, target_app_did, executable_body, params)
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_FILE_UPLOAD:
             data, err_message = {}, None
             if capture_output:
-                data, err_message = run_executable_file_upload(did, app_id, target_did, executable_body, params)
+                data, err_message = run_executable_file_upload(did, app_did, target_did, target_app_did, executable_body, params)
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_FILE_DOWNLOAD:
-            data, err_message = run_executable_file_download(did, app_id, target_did, executable_body, params)
+            data, err_message = run_executable_file_download(did, app_did, target_did, target_app_did, executable_body, params)
             if capture_output:
                 output[SCRIPTING_EXECUTABLE_DOWNLOADABLE] = output_key
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_FILE_PROPERTIES:
-            data, err_message = run_executable_file_properties(did, app_id, target_did, executable_body, params)
+            data, err_message = run_executable_file_properties(did, app_did, target_did, target_app_did, executable_body, params)
         elif executable_type == SCRIPTING_EXECUTABLE_TYPE_FILE_HASH:
-            data, err_message = run_executable_file_hash(did, app_id, target_did, executable_body, params)
+            data, err_message = run_executable_file_hash(did, app_did, target_did, target_app_did, executable_body, params)
         else:
             data, err_message = None, f"invalid executable type '{executable_type}'"
 
@@ -218,6 +212,7 @@ class HiveScripting:
 
         # Data Validation
         executable = content.get('executable')
+        massage_keys_with_dollar_signs(executable)
         err_message = self.__executable_validation(executable)
         if err_message:
             return self.response.response_err(400, err_message)
@@ -296,15 +291,16 @@ class HiveScripting:
         params = content.get('params', None)
         condition = script.get('condition', None)
         if condition:
-            passed = self.__condition_execution(caller_did, caller_app_did, target_did, condition, params)
+            passed = self.__condition_execution(caller_did, caller_app_did, target_did, target_app_did, condition, params)
             if not passed:
                 err_message = f"the conditions were not met to execute this script"
                 return self.response.response_err(403, err_message)
 
         executable = script.get("executable")
+        unmassage_keys_with_dollar_signs(executable)
         output = {}
-        data = self.__executable_execution(caller_did, caller_app_did, target_did, executable, params, output=output)
-
+        data = self.__executable_execution(caller_did, caller_app_did, target_did, target_app_did, executable, params,
+                                           output=output)
         download_file = output.get(SCRIPTING_EXECUTABLE_DOWNLOADABLE, None)
         if download_file:
             data = output.get(download_file)
--- hive/util/did_scripting.py
+++ hive/util/did_scripting.py
@@ -1,11 +1,35 @@
 from flask import request
 
-from hive.util.constants import SCRIPTING_EXECUTABLE_CALLER_DID, SCRIPTING_EXECUTABLE_PARAMS
+from hive.util.constants import SCRIPTING_EXECUTABLE_CALLER_DID, SCRIPTING_EXECUTABLE_PARAMS, \
+    SCRIPTING_EXECUTABLE_CALLER_APP_DID
 from hive.util.did_file_info import query_download, query_properties, query_hash, query_upload_get_filepath
 from hive.util.did_info import get_collection
 from hive.util.did_mongo_db_resource import populate_options_find_many, \
     query_insert_one, query_find_many, populate_options_insert_one, populate_options_count_documents, \
     query_count_documents, populate_options_update_one, query_update_one, query_delete_one
+
+
+def massage_keys_with_dollar_signs(d):
+    for key, value in d.items():
+        if key[0] == "$" and key not in [SCRIPTING_EXECUTABLE_CALLER_DID, SCRIPTING_EXECUTABLE_CALLER_APP_DID,
+                                         SCRIPTING_EXECUTABLE_PARAMS]:
+            d[key.replace("$", "'$'")] = d.pop(key)
+        if type(value) is dict:
+            massage_keys_with_dollar_signs(value)
+        elif type(value) is list:
+            for item in value:
+                massage_keys_with_dollar_signs(item)
+
+
+def unmassage_keys_with_dollar_signs(d):
+    for key, value in d.items():
+        if key[0:3] == "'$'":
+            d[key.replace("'$'", "$")] = d.pop(key)
+        if type(value) is dict:
+            unmassage_keys_with_dollar_signs(value)
+        elif type(value) is list:
+            for item in value:
+                unmassage_keys_with_dollar_signs(item)
 
 
 def check_json_param(content, content_type, args):
@@ -18,11 +42,13 @@ def check_json_param(content, content_type, args):
     return None
 
 
-def run_condition(did, app_id, target_did, condition_body, params):
+def run_condition(did, app_did, target_did, target_app_did, condition_body, params):
     query = {}
     for key, value in condition_body.get('filter').items():
         if value == SCRIPTING_EXECUTABLE_CALLER_DID:
             query[key] = {"$in": [did]}
+        elif value == SCRIPTING_EXECUTABLE_CALLER_APP_DID:
+            query[key] = {"$in": [app_did]}
         elif value.startswith(f"{SCRIPTING_EXECUTABLE_PARAMS}."):
             v = value.replace(f"{SCRIPTING_EXECUTABLE_PARAMS}.", "")
             if not (params and params.get(v, None)):
@@ -34,7 +60,7 @@ def run_condition(did, app_id, target_did, condition_body, params):
 
     options = populate_options_count_documents(condition_body)
 
-    col = get_collection(target_did, app_id, condition_body.get('collection'))
+    col = get_collection(target_did, target_app_did, condition_body.get('collection'))
     data, err_message = query_count_documents(col, condition_body, options)
     if err_message:
         return False
@@ -44,13 +70,15 @@ def run_condition(did, app_id, target_did, condition_body, params):
     return True
 
 
-def run_executable_find(did, app_id, target_did, executable_body, params):
+def run_executable_find(did, app_did, target_did, target_app_did, executable_body, params):
     query = {}
     executable_body_filter = executable_body.get('filter', None)
     if executable_body_filter:
         for key, value in executable_body.get('filter').items():
             if value == SCRIPTING_EXECUTABLE_CALLER_DID:
                 query[key] = {"$in": [did]}
+            elif value == SCRIPTING_EXECUTABLE_CALLER_APP_DID:
+                query[key] = {"$in": [app_did]}
             elif value.startswith(f"{SCRIPTING_EXECUTABLE_PARAMS}."):
                 v = value.replace(f"{SCRIPTING_EXECUTABLE_PARAMS}.", "")
                 if not (params and params.get(v, None)):
@@ -62,7 +90,7 @@ def run_executable_find(did, app_id, target_did, executable_body, params):
 
     options = populate_options_find_many(executable_body)
 
-    col = get_collection(target_did, app_id, executable_body.get('collection'))
+    col = get_collection(target_did, target_app_did, executable_body.get('collection'))
     data, err_message = query_find_many(col, executable_body, options)
     if err_message:
         return None, err_message
@@ -70,12 +98,14 @@ def run_executable_find(did, app_id, target_did, executable_body, params):
     return data, None
 
 
-def run_executable_insert(did, app_id, target_did, executable_body, params):
+def run_executable_insert(did, app_did, target_did, target_app_did, executable_body, params):
     created = False
     query = {}
     for key, value in executable_body.get('document').items():
         if value == SCRIPTING_EXECUTABLE_CALLER_DID:
             query[key] = did
+        elif value == SCRIPTING_EXECUTABLE_CALLER_APP_DID:
+            query[key] = app_did
         else:
             if key == "created":
                 created = True
@@ -91,7 +121,7 @@ def run_executable_insert(did, app_id, target_did, executable_body, params):
 
     options = populate_options_insert_one(executable_body)
 
-    col = get_collection(target_did, app_id, executable_body.get('collection'))
+    col = get_collection(target_did, target_app_did, executable_body.get('collection'))
     data, err_message = query_insert_one(col, executable_body, options, created=created)
     if err_message:
         return None, err_message
@@ -99,11 +129,13 @@ def run_executable_insert(did, app_id, target_did, executable_body, params):
     return data, None
 
 
-def run_executable_update(did, app_id, target_did, executable_body, params):
+def run_executable_update(did, app_did, target_did, target_app_did, executable_body, params):
     filter_query = {}
     for key, value in executable_body.get('filter').items():
         if value == SCRIPTING_EXECUTABLE_CALLER_DID:
             filter_query[key] = did
+        elif value == SCRIPTING_EXECUTABLE_CALLER_APP_DID:
+            filter_query[key] = app_did
         elif value.startswith(f"{SCRIPTING_EXECUTABLE_PARAMS}."):
             v = value.replace(f"{SCRIPTING_EXECUTABLE_PARAMS}.", "")
             if not (params and params.get(v, None)):
@@ -113,9 +145,11 @@ def run_executable_update(did, app_id, target_did, executable_body, params):
             filter_query[key] = value
     executable_body['filter'] = filter_query
     update_set_query = {}
-    for key, value in executable_body.get('update').get("'$set'").items():
+    for key, value in executable_body.get('update').get('$set').items():
         if value == SCRIPTING_EXECUTABLE_CALLER_DID:
             update_set_query[key] = did
+        elif value == SCRIPTING_EXECUTABLE_CALLER_APP_DID:
+            update_set_query[key] = app_did
         elif value.startswith(f"{SCRIPTING_EXECUTABLE_PARAMS}."):
             v = value.replace(f"{SCRIPTING_EXECUTABLE_PARAMS}.", "")
             if not (params and params.get(v, None)):
@@ -123,12 +157,11 @@ def run_executable_update(did, app_id, target_did, executable_body, params):
             update_set_query[key] = params[v]
         else:
             update_set_query[key] = value
-    executable_body['update'].pop("'$set'", None)
     executable_body['update']['$set'] = update_set_query
 
     options = populate_options_update_one(executable_body)
 
-    col = get_collection(target_did, app_id, executable_body.get('collection'))
+    col = get_collection(target_did, target_app_did, executable_body.get('collection'))
     data, err_message = query_update_one(col, executable_body, options)
     if err_message:
         return None, err_message
@@ -136,11 +169,13 @@ def run_executable_update(did, app_id, target_did, executable_body, params):
     return data, None
 
 
-def run_executable_delete(did, app_id, target_did, executable_body, params):
+def run_executable_delete(did, app_did, target_did, target_app_did, executable_body, params):
     query = {}
     for key, value in executable_body.get('filter').items():
         if value == SCRIPTING_EXECUTABLE_CALLER_DID:
             query[key] = {"$in": [did]}
+        elif value == SCRIPTING_EXECUTABLE_CALLER_APP_DID:
+            query[key] = {"$in": [app_did]}
         elif value.startswith(f"{SCRIPTING_EXECUTABLE_PARAMS}."):
             v = value.replace(f"{SCRIPTING_EXECUTABLE_PARAMS}.", "")
             if not (params and params.get(v, None)):
@@ -150,7 +185,7 @@ def run_executable_delete(did, app_id, target_did, executable_body, params):
             query[key] = value
     executable_body["filter"] = query
 
-    col = get_collection(target_did, app_id, executable_body.get('collection'))
+    col = get_collection(target_did, target_app_did, executable_body.get('collection'))
     data, err_message = query_delete_one(col, executable_body)
     if err_message:
         return None, err_message
@@ -158,7 +193,7 @@ def run_executable_delete(did, app_id, target_did, executable_body, params):
     return data, None
 
 
-def run_executable_file_upload(did, app_id, target_did, executable_body, params):
+def run_executable_file_upload(did, app_did, target_did, target_app_did, executable_body, params):
     executable_body_path = executable_body.get("path", "")
     file_name = ""
     if executable_body_path.startswith(f"{SCRIPTING_EXECUTABLE_PARAMS}."):
@@ -167,7 +202,7 @@ def run_executable_file_upload(did, app_id, target_did, executable_body, params)
             return None, "Exception: Parameter is not set"
         file_name = params[v]
 
-    full_path_name, err = query_upload_get_filepath(target_did, app_id, file_name)
+    full_path_name, err = query_upload_get_filepath(target_did, target_app_did, file_name)
     if err:
         return None, f"Exception: Could not upload file. Status={err['status_code']} Error='{err['description']}'"
     try:
@@ -183,7 +218,7 @@ def run_executable_file_upload(did, app_id, target_did, executable_body, params)
     return data, None
 
 
-def run_executable_file_download(did, app_id, target_did, executable_body, params):
+def run_executable_file_download(did, app_did, target_did, target_app_did, executable_body, params):
     executable_body_path = executable_body.get("path", "")
     file_name = ""
     if executable_body_path.startswith(f"{SCRIPTING_EXECUTABLE_PARAMS}."):
@@ -192,13 +227,13 @@ def run_executable_file_download(did, app_id, target_did, executable_body, param
             return None, "Exception: Parameter is not set"
         file_name = params[v]
 
-    data, status_code = query_download(target_did, app_id, file_name)
+    data, status_code = query_download(target_did, target_app_did, file_name)
     if status_code != 200:
         return None, f"Exception: Could not download file. Status={status_code}"
     return data, None
 
 
-def run_executable_file_properties(did, app_id, target_did, executable_body, params):
+def run_executable_file_properties(did, app_did, target_did, target_app_did, executable_body, params):
     executable_body_path = executable_body.get("path", "")
     name = ""
     if executable_body_path.startswith(f"{SCRIPTING_EXECUTABLE_PARAMS}."):
@@ -207,13 +242,13 @@ def run_executable_file_properties(did, app_id, target_did, executable_body, par
             return None, "Exception: Parameter is not set"
         name = params[v]
 
-    data, err = query_properties(target_did, app_id, name)
+    data, err = query_properties(target_did, target_app_did, name)
     if err:
         return None, f"Exception: Could not get properties of file. Status={err['status_code']} Error='{err['description']}'"
     return data, None
 
 
-def run_executable_file_hash(did, app_id, target_did, executable_body, params):
+def run_executable_file_hash(did, app_did, target_did, target_app_did, executable_body, params):
     executable_body_path = executable_body.get("path", "")
     name = ""
     if executable_body_path.startswith(f"{SCRIPTING_EXECUTABLE_PARAMS}."):
@@ -222,7 +257,7 @@ def run_executable_file_hash(did, app_id, target_did, executable_body, params):
             return None, "Exception: Parameter is not set"
         name = params[v]
 
-    data, err = query_hash(target_did, app_id, name)
+    data, err = query_hash(target_did, target_app_did, name)
     if err:
         return None, f"Exception: Could not get hash of file. Status={err['status_code']} Error='{err['description']}'"
     return data, None

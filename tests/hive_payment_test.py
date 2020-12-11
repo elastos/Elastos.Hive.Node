37268ca22b83d06a7f5b4216b1be6fa348ef31dd
patternmap <patternmap@163.com>
2020-12-11 15:09:11 +0800

    CU-5fq0er Add test case for bug backup service

--- tests/hive_payment_test.py
+++ tests/hive_payment_test.py
@@ -53,7 +53,6 @@ class HivePaymentTestCase(unittest.TestCase):
         self.did = test_common.get_auth_did()
         self.app_id = test_common.get_auth_app_did()
         self.test_order_id = None
-        self.init_payment_db()
 
     def init_auth(self):
         token = test_common.get_auth_token()
@@ -66,7 +65,7 @@ class HivePaymentTestCase(unittest.TestCase):
             # self.upload_file_content_type,
         ]
 
-    def init_payment_db(self):
+    def init_vault_payment_db(self):
         connection = MongoClient(host=MONGO_HOST, port=MONGO_PORT)
         db = connection[DID_INFO_DB_NAME]
         order_col = db[VAULT_ORDER_COL]
@@ -82,6 +81,7 @@ class HivePaymentTestCase(unittest.TestCase):
                      VAULT_ORDER_APP_ID: self.app_id,
                      VAULT_ORDER_PACKAGE_INFO: package_info,
                      VAULT_ORDER_TXIDS: ["4813ba481d0e18c4fa03ddc35c32ffbd88080fba14fec8c0c31e5843e6399940"],
+                     VAULT_ORDER_TYPE: VAULT_ORDER_TYPE_VAULT,
                      VAULT_ORDER_STATE: VAULT_ORDER_STATE_CANCELED,
                      VAULT_ORDER_CREATE_TIME: 1591000001,
                      VAULT_ORDER_PAY_TIME: 1591000031,
@@ -93,6 +93,44 @@ class HivePaymentTestCase(unittest.TestCase):
                      VAULT_ORDER_APP_ID: self.app_id,
                      VAULT_ORDER_PACKAGE_INFO: package_info,
                      VAULT_ORDER_TXIDS: [],
+                     VAULT_ORDER_TYPE: VAULT_ORDER_TYPE_VAULT,
+                     VAULT_ORDER_STATE: VAULT_ORDER_STATE_WAIT_PAY,
+                     VAULT_ORDER_CREATE_TIME: (1591703671 + 36288000),
+                     VAULT_ORDER_PAY_TIME: (1591703671 + 36288000),
+                     VAULT_ORDER_MODIFY_TIME: (1591703671 + 36288000)
+                     }
+        ret = order_col.insert_one(order_dic)
+        self.test_order_id = str(ret.inserted_id)
+
+    def init_vault_backup_payment_db(self):
+        connection = MongoClient(host=MONGO_HOST, port=MONGO_PORT)
+        db = connection[DID_INFO_DB_NAME]
+        order_col = db[VAULT_ORDER_COL]
+        query = {VAULT_ORDER_DID: self.did}
+        order_col.delete_many(query)
+        service_col = db[VAULT_SERVICE_COL]
+        service_col.delete_many(query)
+
+        package_info = PaymentConfig.get_backup_plan("Rookie")
+        self.assertNotEqual(package_info, None)
+        # init some cancel order to test cancel order txid use in new order
+        order_dic = {VAULT_ORDER_DID: self.did,
+                     VAULT_ORDER_APP_ID: self.app_id,
+                     VAULT_ORDER_PACKAGE_INFO: package_info,
+                     VAULT_ORDER_TXIDS: ["4813ba481d0e18c4fa03ddc35c32ffbd88080fba14fec8c0c31e5843e6399940"],
+                     VAULT_ORDER_STATE: VAULT_ORDER_STATE_CANCELED,
+                     VAULT_ORDER_TYPE: VAULT_ORDER_TYPE_BACKUP,
+                     VAULT_ORDER_CREATE_TIME: 1591000001,
+                     VAULT_ORDER_PAY_TIME: 1591000031,
+                     VAULT_ORDER_MODIFY_TIME: 1591000031
+                     }
+        order_col.insert_one(order_dic)
+
+        order_dic = {VAULT_ORDER_DID: self.did,
+                     VAULT_ORDER_APP_ID: self.app_id,
+                     VAULT_ORDER_PACKAGE_INFO: package_info,
+                     VAULT_ORDER_TXIDS: [],
+                     VAULT_ORDER_TYPE: VAULT_ORDER_TYPE_BACKUP,
                      VAULT_ORDER_STATE: VAULT_ORDER_STATE_WAIT_PAY,
                      VAULT_ORDER_CREATE_TIME: (1591703671 + 36288000),
                      VAULT_ORDER_PAY_TIME: (1591703671 + 36288000),
@@ -180,8 +218,7 @@ class HivePaymentTestCase(unittest.TestCase):
         )
         self.assert200(s)
         self.assertEqual(r["_status"], "OK")
-        r, msg = can_access_vault(self.did, VAULT_ACCESS_WR)
-        self.assertTrue(r)
+        self.assertTrue(can_access_vault(self.did, VAULT_ACCESS_WR))
 
     def test_2_create_package_order(self):
         logging.getLogger("").debug("\nRunning test_2_create_package_order")
@@ -218,6 +255,7 @@ class HivePaymentTestCase(unittest.TestCase):
 
     def test_4_pay_and_start_package_order(self):
         logging.getLogger("").debug("\nRunning test_4_pay_and_start_package_order")
+        self.init_vault_payment_db()
         pay_param = {
             "order_id": self.test_order_id,
             "pay_txids": ["4813ba481d0e18c4fa03ddc35c32ffbd88080fba14fec8c0c31e5843e6399940"]
@@ -232,22 +270,18 @@ class HivePaymentTestCase(unittest.TestCase):
         self.assertEqual(r["_status"], "OK")
 
         check_wait_order_tx_job()
-        r, msg = can_access_vault(self.did, VAULT_ACCESS_WR)
-        self.assertTrue(r)
+        self.assertTrue(can_access_vault(self.did, VAULT_ACCESS_WR))
 
     def test_5_service_storage(self):
         test_common.setup_test_vault(self.did)
         count_vault_storage_job()
-        r, msg = can_access_vault(self.did, VAULT_ACCESS_WR)
-        self.assertTrue(r)
+        self.assertTrue(can_access_vault(self.did, VAULT_ACCESS_WR))
         inc_vault_file_use_storage_byte(self.did, 55000000)
         update_vault_db_use_storage_byte(self.did, 55000000)
-        r, msg = can_access_vault(self.did, VAULT_ACCESS_WR)
-        self.assertFalse(r)
+        self.assertFalse(can_access_vault(self.did, VAULT_ACCESS_WR))
         inc_vault_file_use_storage_byte(self.did, -20000000)
         update_vault_db_use_storage_byte(self.did, 30000000)
-        r, msg = can_access_vault(self.did, VAULT_ACCESS_WR)
-        self.assertTrue(r)
+        self.assertTrue(can_access_vault(self.did, VAULT_ACCESS_WR))
 
     def assert_service_vault_info(self, state):
         r, s = self.parse_response(
@@ -265,14 +299,62 @@ class HivePaymentTestCase(unittest.TestCase):
         self.assert_service_vault_info("Rookie")
         inc_vault_file_use_storage_byte(self.did, 910000000)
         update_vault_db_use_storage_byte(self.did, 910000000)
-        r, msg = can_access_vault(self.did, VAULT_ACCESS_WR)
-        self.assertTrue(r)
+        self.assertTrue(can_access_vault(self.did, VAULT_ACCESS_WR))
         proc_expire_vault_job()
         self.assert_service_vault_info("Free")
-        r, msg = can_access_vault(self.did, VAULT_ACCESS_R)
-        self.assertTrue(r)
-        r, msg = can_access_vault(self.did, VAULT_ACCESS_WR)
-        self.assertFalse(r)
+        self.assertTrue(can_access_vault(self.did, VAULT_ACCESS_R))
+        self.assertFalse(can_access_vault(self.did, VAULT_ACCESS_WR))
+
+    def test_7_create_package_order(self):
+        logging.getLogger("").debug("\nRunning test_2_create_package_order")
+
+        package = {
+            "backup_name": "Rookie"
+        }
+
+        r, s = self.parse_response(
+            self.test_client.post('/api/v1/payment/create_vault_package_order',
+                                  data=json.dumps(package),
+                                  headers=self.auth)
+        )
+        self.assert200(s)
+        self.assertEqual(r["_status"], "OK")
+
+        order_id = r["order_id"]
+
+        r, s = self.parse_response(
+            self.test_client.get('api/v1/payment/vault_package_order?order_id=' + order_id, headers=self.auth)
+        )
+        self.assert200(s)
+        self.assertEqual(r["_status"], "OK")
+        print(r)
+
+    def test_8_pay_and_start_backup_order(self):
+        logging.getLogger("").debug("\nRunning test_4_pay_and_start_package_order")
+        self.init_vault_backup_payment_db()
+        pay_param = {
+            "order_id": self.test_order_id,
+            "pay_txids": ["4813ba481d0e18c4fa03ddc35c32ffbd88080fba14fec8c0c31e5843e6399940"]
+        }
+
+        r, s = self.parse_response(
+            self.test_client.post('/api/v1/payment/pay_vault_package_order',
+                                  data=json.dumps(pay_param),
+                                  headers=self.auth)
+        )
+        self.assert200(s)
+        self.assertEqual(r["_status"], "OK")
+
+        check_wait_order_tx_job()
+        self.assert_service_vault_backup_info("Rookie")
+
+    def assert_service_vault_backup_info(self, state):
+        r, s = self.parse_response(
+            self.test_client.get('api/v1/service/vault/backup', headers=self.auth)
+        )
+        self.assert200(s)
+        self.assertEqual(r["_status"], "OK")
+        self.assertEqual(r["vault_service_info"][VAULT_BACKUP_SERVICE_USING], state)
 
 
 if __name__ == '__main__':

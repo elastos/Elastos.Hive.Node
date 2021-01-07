889a465793579370ccffe16802ec4822e3c27706
patternmap <patternmap@163.com>
2021-01-07 17:08:29 +0800

    CU-5vxv25 rebuild code for log can output by gunicorn -b 0.0.0.0:5000 'hive:create_app()'

--- hive/main/hive_manage.py
+++ hive/main/hive_manage.py
@@ -1,6 +1,10 @@
 from hive.settings import HIVE_VERSION, HIVE_COMMIT_HASH
 from hive.util.server_response import ServerResponse
 
+import logging
+
+logger = logging.getLogger("HiveManage")
+
 
 class HiveManage:
     def __init__(self):
@@ -12,8 +16,12 @@ class HiveManage:
 
     def get_hive_version(self):
         data = {"version": HIVE_VERSION}
+        print("version:" + HIVE_VERSION)
+        logger.debug("version:" + HIVE_VERSION)
         return self.response.response_ok(data)
 
     def get_hive_commit_hash(self):
         data = {"commit_hash": HIVE_COMMIT_HASH}
+        print("commit_hash:" + HIVE_COMMIT_HASH)
+        logger.debug("commit_hash:" + HIVE_COMMIT_HASH)
         return self.response.response_ok(data)

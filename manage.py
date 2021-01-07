3d5eb876c09b3c10680207600f070ed81f5b8f45
patternmap <patternmap@163.com>
2021-01-07 17:08:29 +0800

    CU-5vxv25 rebuild code for log can output by gunicorn -b 0.0.0.0:5000 'hive:create_app()'

--- manage.py
+++ manage.py
@@ -1,18 +1,7 @@
-import logging.config, yaml
-
 from flask import request
 from flask_script import Manager, Server
 
 from hive import create_app
-
-logging.config.dictConfig(yaml.load(open('logging.conf'), Loader=yaml.FullLoader))
-logfile = logging.getLogger('file')
-log_console = logging.getLogger('console')
-logfile.debug("Debug FILE")
-log_console.debug("Debug CONSOLE")
-
-
-
 
 manager = Manager(create_app)
 manager.add_command("runserver", Server(host="0.0.0.0", port=5000))

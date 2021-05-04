a37170539f309de0f12f2e3b78147552e6807e6e
Fred Chen <chentj070921@yeah.net>
2021-05-04 12:10:36 +0800

    CU-u74h6c - Refine run-script API version 2 for find and insert document of the script.

--- hive/util/database_client.py
+++ hive/util/database_client.py
@@ -6,12 +6,13 @@ Any database operations can be found here.
 
 from hive.settings import hive_setting
 from pymongo import MongoClient
-from hive.util.did_mongo_db_resource import gene_mongo_db_name
+from hive.util.did_mongo_db_resource import gene_mongo_db_name, convert_oid
 from hive.util.constants import DID_INFO_DB_NAME, VAULT_SERVICE_COL, VAULT_SERVICE_DID, VAULT_SERVICE_STATE, \
     VAULT_SERVICE_MAX_STORAGE, VAULT_SERVICE_START_TIME, VAULT_SERVICE_END_TIME, VAULT_SERVICE_PRICING_USING, \
     VAULT_ACCESS_WR, DID, APP_ID, VAULT_SERVICE_FILE_USE_STORAGE, VAULT_SERVICE_DB_USE_STORAGE, \
-    VAULT_SERVICE_MODIFY_TIME, VAULT_ACCESS_DEL
-from hive.util.http_response import NotFoundException, ErrorCode
+    VAULT_SERVICE_MODIFY_TIME, VAULT_ACCESS_DEL, DATETIME_FORMAT
+from hive.util.http_response import NotFoundException, ErrorCode, BadRequestException
+from datetime import datetime
 
 
 VAULT_SERVICE_FREE = "Free"
@@ -52,6 +53,28 @@ class DatabaseClient:
         if (access_vault == VAULT_ACCESS_WR or access_vault == VAULT_ACCESS_DEL)\
                 and info[VAULT_SERVICE_STATE] == VAULT_SERVICE_STATE_FREEZE:
             raise NotFoundException(ErrorCode.VAULT_NO_PERMISSION, "The vault can't be written.")
+
+    def find_many(self, did, app_id, collection_name, col_filter, options):
+        col = self.get_user_collection(did, app_id, collection_name)
+        if not col:
+            raise BadRequestException(msg='Cannot find collection with name ' + collection_name)
+
+        return list(col.find(convert_oid(col_filter) if col_filter else None, **options))
+
+    def insert_one(self, did, app_id, collection_name, document, options):
+        col = self.get_user_collection(did, app_id, collection_name)
+        if not col:
+            raise BadRequestException(msg='Cannot find collection with name ' + collection_name)
+
+        document['created'] = datetime.strptime(document["created"], DATETIME_FORMAT)\
+            if 'created' in document else datetime.utcnow()
+        document['modified'] = datetime.utcnow()
+        ret = col.insert_one(convert_oid(document, **options))
+
+        return {
+            "acknowledged": ret.acknowledged,
+            "inserted_id": str(ret.inserted_id)
+        }
 
 
 cli = DatabaseClient()

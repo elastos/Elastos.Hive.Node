f18d75deb5d8fd2d2c936888010218f88d3c8b32
Fred Chen <chentj070921@yeah.net>
2021-05-17 14:09:34 +0800

    CU-8hwt8g - Add the definition to the subscription module.

---
+++ src/modules/subscription/subscription.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+"""
+Entrance of the subscription module.
+"""
+
+
+class VaultSubscription:
+    def __init__(self):
+        pass
+
+    def subscribe(self, credential):
+        pass
+
+    def unsubscribe(self):
+        pass
+
+    def activate(self):
+        pass
+
+    def deactivate(self):
+        pass
+
+    def get_info(self):
+        pass
+
+
+class BackupSubscription:
+    def __init__(self):
+        pass
+
+    def subscribe(self, credential):
+        pass
+
+    def unsubscribe(self):
+        pass
+
+    def activate(self):
+        pass
+
+    def deactivate(self):
+        pass
+
+    def get_info(self):
+        pass

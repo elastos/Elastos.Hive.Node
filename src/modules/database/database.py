f8bbe39ae01b5065075bc7905994b1e9d31a95e2
Fred Chen <chentj070921@yeah.net>
2021-05-21 09:34:33 +0800

    CU-8m1cw6 - Support create/delete collection for database module.

--- src/modules/database/database.py
+++ src/modules/database/database.py
@@ -3,6 +3,11 @@
 """
 The entrance for database module.
 """
+from hive.util.constants import VAULT_ACCESS_WR, VAULT_ACCESS_DEL
+from hive.util.did_mongo_db_resource import get_mongo_database_size
+from hive.util.payment.vault_service_manage import update_vault_db_use_storage_byte
+from src.modules.scripting.scripting import check_auth_and_vault
+from src.utils.database_client import cli
 
 
 class Database:
@@ -10,10 +15,16 @@ class Database:
         pass
 
     def create_collection(self, collection_name):
-        pass
+        did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
+        cli.create_collection(did, app_did, collection_name)
+        return {
+            'name': collection_name
+        }
 
     def delete_collection(self, collection_name):
-        pass
+        did, app_did = check_auth_and_vault(VAULT_ACCESS_DEL)
+        cli.delete_collection(did, app_did, collection_name)
+        update_vault_db_use_storage_byte(did, get_mongo_database_size(did, app_did))
 
     def insert_document(self, collection_name, json_body):
         pass

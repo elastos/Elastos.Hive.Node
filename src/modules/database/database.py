78241cea41d8fd0fcfc23e3045937789baf7746d
Fred Chen <chentj070921@yeah.net>
2021-05-21 09:10:47 +0800

    CU-8m1cw6 - Add view definition for database module.

---
+++ src/modules/database/database.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+The entrance for database module.
+"""
+
+
+class Database:
+    def __init__(self):
+        pass
+
+    def create_collection(self, collection_name):
+        pass
+
+    def delete_collection(self, collection_name):
+        pass
+
+    def insert_document(self, collection_name, json_body):
+        pass
+
+    def update_document(self, collection_name, json_body):
+        pass
+
+    def delete_document(self, collection_name, json_body):
+        pass
+
+    def find_document(self, collection_name, col_filter, skip, limit):
+        pass
+
+    def query_document(self, collection_name, json_body):
+        pass

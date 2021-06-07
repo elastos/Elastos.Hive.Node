01a4519ad25d1a16706dae4bf7724e1a1150007b
Fred Chen <chentj070921@yeah.net>
2021-06-07 11:15:07 +0800

    CU-8xwppu - Update did/ela url.

--- tests/database_test.py
+++ tests/database_test.py
@@ -12,7 +12,6 @@ from tests.utils.http_client import HttpClient
 from tests import init_test
 
 
-@unittest.skip
 class DatabaseTestCase(unittest.TestCase):
     def __init__(self, method_name='runTest'):
         super().__init__(method_name)

3a31f557828be033da641d43cb5ff9051852ce8f
Fred Chen <chentj070921@yeah.net>
2021-06-08 16:04:40 +0800

    CU-8xwppu - Add the definition of backup APIs.

--- src/view/__init__.py
+++ src/view/__init__.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 from hive.settings import hive_setting
-from src.view import scripting
-from src.view import subscription
-from src.view import files
-from src.view import database
-from src.view import auth
+from src.view import scripting, subscription, files, database, auth, backup
 
 
 def init_app(app, mode):
-    scripting.init_app(app, hive_setting)
+    auth.init_app(app, hive_setting)
     subscription.init_app(app, hive_setting)
+    backup.init_app(app, hive_setting)
+    scripting.init_app(app, hive_setting)
     files.init_app(app, hive_setting)
     database.init_app(app, hive_setting)
-    auth.init_app(app, hive_setting)

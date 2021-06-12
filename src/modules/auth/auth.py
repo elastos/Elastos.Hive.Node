87bb837e38dd4b4a2acfa5e26113ddf4f19d091d
Fred Chen <chentj070921@yeah.net>
2021-06-12 09:26:07 +0800

    CU-95wju9 - Add the implementation of vault backup API.

--- src/modules/auth/auth.py
+++ src/modules/auth/auth.py
@@ -14,7 +14,10 @@ from hive.util.constants import APP_INSTANCE_DID, DID_INFO_NONCE_EXPIRED
 from hive.util.did.entity import Entity
 from hive.util.did_info import create_nonce, get_did_info_by_app_instance_did, add_did_nonce_to_db, \
     update_did_info_by_app_instance_did, get_did_info_by_nonce, update_token_of_did_info
-from src.utils.http_response import hive_restful_response, BadRequestException
+from hive.main import view
+
+from src.utils.http_response import hive_restful_response, BadRequestException, InvalidParameterException
+from src.view import URL_DID_SIGN_IN, URL_DID_BACKUP_AUTH
 
 
 class Auth(Entity):
@@ -220,3 +223,85 @@ class Auth(Entity):
         """ helper method to get error message from did.so """
         err_message = ffi.string(lib.DIDError_GetMessage()).decode()
         return err_message if not prompt else f'[{prompt}] {err_message}'
+
+    def get_backup_credential_info(self, credential):
+        """ for vault /backup """
+        if not credential:
+            raise InvalidParameterException()
+        credential_info, err = view.h_auth.get_credential_info(credential, ["targetHost", "targetDID"])
+        if credential_info is None:
+            raise InvalidParameterException(msg=err)
+        return credential_info
+
+    def backup_sign_in(self, host_url, credential, subject):
+        """
+        for vault /backup & /restore
+        :return challenge_response, backup_service_instance_did
+        """
+        vc = lib.Credential_FromJson(credential.encode(), ffi.NULL)
+        if not vc:
+            raise InvalidParameterException(msg='backup_sign_in: invalid credential.')
+
+        doc_str = ffi.string(lib.DIDDocument_ToJson(lib.DIDStore_LoadDID(self.store, self.did), True)).decode()
+        doc = json.loads(doc_str)
+        rt, status_code, err = self.post(host_url + URL_DID_SIGN_IN, {"id": doc})
+        if err or 'challenge' not in rt or not rt["challenge"]:
+            raise InvalidParameterException(msg='backup_sign_in: failed to sign in to backup node.')
+
+        jws = lib.DefaultJWSParser_Parse(rt["challenge"].encode())
+        if not jws:
+            raise InvalidParameterException(
+                msg=f'backup_sign_in: failed to parse challenge with error {self.get_error_message()}.')
+
+        aud = ffi.string(lib.JWT_GetAudience(jws)).decode()
+        if aud != self.get_did_string():
+            lib.JWT_Destroy(jws)
+            raise InvalidParameterException(msg=f'backup_sign_in: failed to get the audience of the challenge.')
+
+        nonce = ffi.string(lib.JWT_GetClaim(jws, "nonce".encode())).decode()
+        if nonce is None:
+            lib.JWT_Destroy(jws)
+            raise InvalidParameterException(
+                msg=f'backup_sign_in: failed to get the nonce of the challenge.')
+
+        issuer = ffi.string(lib.JWT_GetIssuer(jws)).decode()
+        lib.JWT_Destroy(jws)
+        if issuer is None:
+            raise InvalidParameterException(
+                msg=f'backup_sign_in: failed to get the issuer of the challenge.')
+
+        vp_json = self.create_presentation(vc, nonce, issuer)
+        if vp_json is None:
+            raise InvalidParameterException(
+                msg=f'backup_sign_in: failed to create presentation.')
+        challenge_response = self.create_vp_token(vp_json, subject, issuer, self.hive_setting.AUTH_CHALLENGE_EXPIRED)
+        if challenge_response is None:
+            raise InvalidParameterException(
+                msg=f'backup_sign_in: failed to create the challenge response.')
+        return challenge_response, issuer
+
+    def backup_auth(self, host_url, challenge_response, backup_service_instance_did):
+        """
+        for vault /backup & /restore
+        :return backup access token
+        """
+        rt, _, err = self.post(host_url + URL_DID_BACKUP_AUTH, {"challenge_response": challenge_response})
+        if err or 'token' not in rt or not rt["backup_token"]:
+            raise InvalidParameterException(msg='backup_auth: failed to backup auth to backup node.')
+
+        jws = lib.DefaultJWSParser_Parse(rt["token"].encode())
+        if not jws:
+            raise InvalidParameterException(
+                msg=f'backup_auth: failed to parse token with error {self.get_error_message()}.')
+
+        audience = ffi.string(lib.JWT_GetAudience(jws)).decode()
+        if audience != self.get_did_string():
+            lib.JWT_Destroy(jws)
+            raise InvalidParameterException(msg=f'backup_auth: failed to get the audience of the challenge.')
+
+        issuer = ffi.string(lib.JWT_GetIssuer(jws)).decode()
+        lib.JWT_Destroy(jws)
+        if issuer != backup_service_instance_did:
+            raise InvalidParameterException(msg=f'backup_auth: failed to get the issuer of the challenge.')
+
+        return rt["backup_token"]

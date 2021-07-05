6f6abd353dadec2401e0a969f4668ce355b11c13
Fred Chen <chentj070921@yeah.net>
2021-07-05 14:40:11 +0800

    CU-9zyxa9 - Fix issue for the size of the vault information.

--- hive/main/hive_payment.py
+++ hive/main/hive_payment.py
@@ -185,6 +185,8 @@ class HivePayment:
         else:
             del info["_id"]
             data = dict()
+            info[VAULT_SERVICE_MAX_STORAGE] = info[VAULT_SERVICE_MAX_STORAGE] \
+                if info[VAULT_SERVICE_MAX_STORAGE] < 1024*1024 else info[VAULT_SERVICE_MAX_STORAGE] / (1024 * 1024)
             info[VAULT_SERVICE_FILE_USE_STORAGE] = info[VAULT_SERVICE_FILE_USE_STORAGE] / (1024 * 1024)
             info[VAULT_SERVICE_DB_USE_STORAGE] = info[VAULT_SERVICE_DB_USE_STORAGE] / (1024 * 1024)
             data["vault_service_info"] = info

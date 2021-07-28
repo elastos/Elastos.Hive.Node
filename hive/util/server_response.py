74c3db1bb266f52e08dd0503c20c78bb62836a52
Fred Chen <chentj070921@yeah.net>
2021-07-28 10:47:39 +0800

    CU-ap3hw0 - Refine the success response logic for well output.

--- hive/util/server_response.py
+++ hive/util/server_response.py
@@ -14,11 +14,14 @@ class ServerResponse:
 
     def response_ok(self, data_dic=None):
         ret = {STATUS: STATUS_OK}
-        if data_dic is not None:
-            try:
-                ret.update(data_dic)
-            except Exception as e:
-                return self.response_err(400, f'invalid response body: {str(data_dic)}, {str(e)}')
+        if not isinstance(data_dic, dict):
+            return self.response_err(500, f'impossible response body: {str(data_dic)}')
+
+        try:
+            ret.update(data_dic)
+        except Exception as e:
+            return self.response_err(400, f'invalid response body: {str(data_dic)}, {str(e)}')
+
         self.logger.debug(json.dumps(ret))
         return jsonify(ret)
 

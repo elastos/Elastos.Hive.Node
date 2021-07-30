c604f4b2a93acd12cd9224d3329a56d7dbee9815
Fred Chen <chentj070921@yeah.net>
2021-07-30 11:32:59 +0800

    CU-atxvue - remove 'show page source' on the index of docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,6 +65,9 @@ exclude_patterns = []
 # html_theme = "classic"
 html_theme = "sphinx_rtd_theme"
 
+# for theme "sphinx_rtd_theme"
+html_show_sourcelink = False
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

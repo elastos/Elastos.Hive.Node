be00b8d576dbbe3d07324fe1599cae2220069793
Fred Chen <chentj070921@yeah.net>
2021-08-09 23:39:21 +0800

    CU-atxvue - Add the docs for the scripting module.

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -14,10 +14,14 @@ Summary
 .. qrefflask:: src:make_port(is_first=True)
   :undoc-static:
   :endpoints: auth.did_sign_in, auth.did_auth,
-    subscription.vault_subscribe,
+    subscription.vault_subscribe, subscription.vault_unsubscribe, subscription.vault_get_info,
+    subscription.backup_subscribe, subscription.backup_unsubscribe, subscription.backup_get_info,
+    subscription.vault_get_price_plan,
     database.create_collection, database.delete_collection, database.insert_or_count_document,
     database.update_document, database.delete_document, database.find_document, database.query_document,
-    files.reading_operation, files.writing_operation, files.move_file, files.delete_file
+    files.reading_operation, files.writing_operation, files.move_file, files.delete_file,
+    scripting.register_script, scripting.call_script, scripting.call_script_url, scripting.delete_script,
+    scripting.upload_file, scripting.download_file
 
 01 Auth
 =======
@@ -52,12 +56,54 @@ auth
 
 subscription for the vault service and the backup service.
 
-subscribe
----------
+vault subscribe
+---------------
 
 .. autoflask:: src:make_port()
   :undoc-static:
   :endpoints: subscription.vault_subscribe
+
+vault unsubscribe
+-----------------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: subscription.vault_unsubscribe
+
+get vault info.
+---------------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: subscription.vault_get_info
+
+backup subscribe
+----------------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: subscription.backup_subscribe
+
+backup unsubscribe
+------------------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: subscription.backup_unsubscribe
+
+get backup info.
+----------------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: subscription.backup_get_info
+
+get pricing plans
+-----------------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: subscription.vault_get_price_plan
 
 03 Database
 ===========
@@ -145,3 +191,52 @@ delete
 .. autoflask:: src:make_port()
   :undoc-static:
   :endpoints: files.delete_file
+
+05 Scripting
+============
+
+The scripting module supports share the data of the vault service for other users.
+
+register script
+---------------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: scripting.register_script
+
+call script
+-----------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: scripting.call_script
+
+call script url
+---------------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: scripting.call_script_url
+
+unregister script
+-----------------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: scripting.delete_script
+
+upload file
+-----------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: scripting.upload_file
+
+download file
+-------------
+
+.. autoflask:: src:make_port()
+  :undoc-static:
+  :endpoints: scripting.download_file
+
+
--- src/view/scripting.py
+++ src/view/scripting.py
@@ -21,21 +21,305 @@ def init_app(app, hive_setting):
 
 @blueprint.route('/api/v2/vault/scripting/<script_name>', methods=['PUT'])
 def register_script(script_name):
+    """ Register a new script for the vault data owner by the script name.
+
+    Script caller will run the script by name later.
+    The script is treated as the channel for other users to access the owner's data.
+    This will set up a condition and an executable.
+    The condition is checked and must matches before running the executable.
+    What the executable can do depends on the type of it.
+    For example, the type "find" can query the documents from a collection.
+
+    .. :quickref: 05 Scripting; Register
+
+    **Request**:
+
+    .. code-block:: json
+
+        {
+            "name": "get_messages",
+            "condition": {
+                "type": "queryHasResult",
+                "name": "verify_user_permission",
+                "body": {
+                    "collection": "groups",
+                    "filter": {
+                        "_id": "$params.group_id",
+                        "friends": "$caller_did"
+                    }
+                }
+            },
+            "executable": {
+                "type": "find",
+                "name": "find_messages",
+                "output": true,
+                "body": {
+                    "collection": "messages",
+                    "filter": {
+                        "group_id": "$params.group_id"
+                    },
+                    "options": {
+                        "projection": {
+                            "_id": false
+                        },
+                        "limit": 100
+                    }
+                }
+            }
+        }
+
+    **Response OK**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 200 OK
+
+    .. code-block:: json
+
+        {
+            "find_messages": {
+                "acknowledged":true,
+                "matched_count":1,
+                "modified_count":1,
+                "upserted_id":null
+            }
+        }
+
+    **Response Error**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 401 Unauthorized
+
+    .. sourcecode:: http
+
+        HTTP/1.1 400 Bad Request
+
+    **Condition**
+
+    There are three types of conditions: 'and', 'or', 'queryHasResult'. The 'and' and the 'or' are for merging
+    other type conditions and can be recursive. 'queryHasResult' is for checking
+    whether the document can be found in the collection by a filter. Here is an example for three types:
+
+    .. code-block:: json
+
+        {
+           "condition":{
+              "type":"and",
+              "name":"verify_user_permission",
+              "body":[
+                 {
+                    "type":"or",
+                    "name":"verify_user_permission",
+                    "body":[
+                       {
+                          "type":"queryHasResult",
+                          "name":"user_in_group",
+                          "body":{
+                             "collection":"groups",
+                             "filter":{
+                                "_id":"$params.group_id",
+                                "friends":"$caller_did"
+                             }
+                          }
+                       },
+                       {
+                          "type":"queryHasResult",
+                          "name":"user_in_group",
+                          "body":{
+                             "collection":"groups",
+                             "filter":{
+                                "_id":"$params.group_id",
+                                "friends":"$caller_did"
+                             }
+                          }
+                       }
+                    ]
+                 },
+                 {
+                    "type":"queryHasResult",
+                    "name":"user_in_group",
+                    "body":{
+                       "collection":"groups",
+                       "filter":{
+                          "_id":"$params.group_id",
+                          "friends":"$caller_did"
+                       }
+                    }
+                 }
+              ]
+           }
+        }
+
+
+    **Executable**
+
+    There are nine types of executables. Here lists all types with the relating examples.
+    For the request params and the response, please check Run Script for how to use them.
+    No response will be provided if the output option sets to false.
+
+    Possible executable types are here:
+
+    - aggregated
+    - find
+    - insert
+    - update
+    - delete
+    - fileUpload
+    - fileDownload
+    - fileProperties
+    - fileHash
+
+    """
     return scripting.set_script(script_name)
 
 
 @blueprint.route('/api/v2/vault/scripting/<script_name>', methods=['DELETE'])
 def delete_script(script_name):
+    """ Remove the script by the script name and the script can not be called anymore.
+
+    .. :quickref: 05 Scripting; Unregister
+
+    **Request**:
+
+    .. code-block:: json
+
+        None
+
+    **Response OK**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 204 No Content
+
+    **Response Error**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 401 Unauthorized
+
+    .. sourcecode:: http
+
+        HTTP/1.1 400 Bad Request
+
+    .. sourcecode:: http
+
+        HTTP/1.1 404 Not Found
+
+    """
     return scripting.delete_script(script_name)
 
 
 @blueprint.route('/api/v2/vault/scripting/<script_name>', methods=['PATCH'])
 def call_script(script_name):
+    """ Run the script registered by the owner.
+
+    Before running the script, the caller needs to check if matches the script condition.
+    The parameter 'context' is also required for tell the scripting service
+    which did and app_did is the data belongs to.
+
+    The 'params' parameter is used to provide the value which the script requires if exists.
+
+    .. :quickref: 05 Scripting; Run Script
+
+    **Request**:
+
+    .. code-block:: json
+
+        {
+            "context": {
+                "target_did": "did:elastos:icXtpDnZRSDrjmD5NQt6TYSphFRqoo2q6n",
+                "target_app_did":"appId"
+            },
+            "params": {
+                "group_id": {"$oid": "5f8d9dfe2f4c8b7a6f8ec0f1"}
+            }
+        }
+
+    **Response OK**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 200 OK
+
+    .. code-block:: json
+
+        {
+           "get_groups":{
+              "items":[
+                 {
+                    "name":"Tuum Tech"
+                 }
+              ]
+           }
+        }
+
+    **Response Error**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 401 Unauthorized
+
+    .. sourcecode:: http
+
+        HTTP/1.1 400 Bad Request
+
+    .. sourcecode:: http
+
+        HTTP/1.1 404 Not Found
+
+    """
     return scripting.run_script(script_name)
 
 
 @blueprint.route('/api/v2/vault/scripting/<script_name>/<context_str>/<params>', methods=['GET'])
 def call_script_url(script_name, context_str, params):
+    """ Run the script registered by the owner by the URL parameters.
+
+    This is the same as **Run Script**.
+
+    .. :quickref: 05 Scripting; Run Script URL
+
+    **URL Parameters**:
+
+    .. sourcecode:: http
+
+        <context_str> # context for running the script.
+        <params> # params for running the script.
+
+    **Response OK**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 200 OK
+
+    .. code-block:: json
+
+        {
+           "get_groups":{
+              "items":[
+                 {
+                    "name":"Tuum Tech"
+                 }
+              ]
+           }
+        }
+
+    **Response Error**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 401 Unauthorized
+
+    .. sourcecode:: http
+
+        HTTP/1.1 400 Bad Request
+
+    .. sourcecode:: http
+
+        HTTP/1.1 404 Not Found
+
+    """
     target_did, target_app_did = None, None
     parts = context_str.split('@')
     if len(parts) == 2 and parts[0] and parts[1]:
@@ -45,9 +329,75 @@ def call_script_url(script_name, context_str, params):
 
 @blueprint.route('/api/v2/vault/scripting/stream/<transaction_id>', methods=['PUT'])
 def upload_file(transaction_id):
+    """ Upload file by transaction id returned by the running script for the executable type 'fileUpload'.
+
+    .. :quickref: 05 Scripting; Upload File
+
+    **Request**:
+
+    .. sourcecode:: http
+
+        <The bytes content of the file>
+
+    **Response OK**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 200 OK
+
+    **Response Error**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 401 Unauthorized
+
+    .. sourcecode:: http
+
+        HTTP/1.1 400 Bad Request
+
+    .. sourcecode:: http
+
+        HTTP/1.1 404 Not Found
+
+    """
     return scripting.upload_file(transaction_id)
 
 
 @blueprint.route('/api/v2/vault/scripting/stream/<transaction_id>', methods=['GET'])
 def download_file(transaction_id):
+    """ Download file by transaction id which is returned by running script for the executable type 'fileDownload'.
+
+    .. :quickref: 05 Scripting; Download File
+
+    **Request**:
+
+    .. sourcecode:: http
+
+        None
+
+    **Response OK**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 200 OK
+
+    .. code-block:: json
+
+        <The bytes content of the file>
+
+    **Response Error**:
+
+    .. sourcecode:: http
+
+        HTTP/1.1 401 Unauthorized
+
+    .. sourcecode:: http
+
+        HTTP/1.1 400 Bad Request
+
+    .. sourcecode:: http
+
+        HTTP/1.1 404 Not Found
+
+    """
     return scripting.download_file(transaction_id)

733c3bbfe0a888b13d710aaba56815bc57fbf229
Fred Chen <chentj070921@yeah.net>
2021-09-30 20:47:22 +0800

    CU-1m8hnbw - Rename 'did' to specific name, such as 'user_did', in codes.

--- src/modules/files/files.py
+++ src/modules/files/files.py
@@ -25,29 +25,29 @@ class Files:
     def __init__(self):
         pass
 
-    def _get_file_full_path(self, did, app_did, path):
+    def _get_file_full_path(self, user_did, app_did, path):
         fixed_path = filter_path_root(path)
-        return (get_save_files_path(did, app_did) / fixed_path).resolve()
+        return (get_save_files_path(user_did, app_did) / fixed_path).resolve()
 
     @hive_restful_response
     def upload_file(self, path):
         if not path:
             raise InvalidParameterException()
 
-        did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
-        self.upload_file_by_did(did, app_did, path)
+        user_did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
+        self.upload_file_by_did(user_did, app_did, path)
         return {
             'name': path
         }
 
-    def upload_file_by_did(self, did, app_did, path):
-        full_path, old_file_size = self._upload_file_from_request_stream(did, app_did, path)
+    def upload_file_by_did(self, user_did, app_did, path):
+        full_path, old_file_size = self._upload_file_from_request_stream(user_did, app_did, path)
         inc_size = os.path.getsize(full_path.as_posix()) - old_file_size
         if inc_size != 0:
-            inc_vault_file_use_storage_byte(did, inc_size)
+            inc_vault_file_use_storage_byte(user_did, inc_size)
 
-    def _upload_file_from_request_stream(self, did, app_did, path):
-        full_path, err = query_upload_get_filepath(did, app_did, path)
+    def _upload_file_from_request_stream(self, user_did, app_did, path):
+        full_path, err = query_upload_get_filepath(user_did, app_did, path)
         if err:
             raise BadRequestException(msg=f'Failed to get upload file full path: "{str(err)}"')
 
@@ -77,11 +77,11 @@ class Files:
         if not path:
             raise InvalidParameterException()
 
-        did, app_did = check_auth_and_vault(VAULT_ACCESS_R)
-        return self.download_file_by_did(did, app_did, path)
+        user_did, app_did = check_auth_and_vault(VAULT_ACCESS_R)
+        return self.download_file_by_did(user_did, app_did, path)
 
-    def download_file_by_did(self, did, app_did, path):
-        data, status_code = query_download(did, app_did, path)
+    def download_file_by_did(self, user_did, app_did, path):
+        data, status_code = query_download(user_did, app_did, path)
         if status_code == BAD_REQUEST:
             raise FileNotFoundException(msg='Cannot get file name by transaction id')
         elif status_code == NOT_FOUND:
@@ -95,17 +95,17 @@ class Files:
         if not path:
             raise InvalidParameterException()
 
-        did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
-        full_path = self._get_file_full_path(did, app_did, path)
+        user_did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
+        full_path = self._get_file_full_path(user_did, app_did, path)
         if full_path.exists():
             if full_path.is_dir():
                 dir_size = get_dir_size(full_path.as_posix(), 0.0)
                 shutil.rmtree(full_path)
-                inc_vault_file_use_storage_byte(did, -dir_size)
+                inc_vault_file_use_storage_byte(user_did, -dir_size)
             else:
                 file_size = os.path.getsize(full_path.as_posix())
                 full_path.unlink()
-                inc_vault_file_use_storage_byte(did, -file_size)
+                inc_vault_file_use_storage_byte(user_did, -file_size)
 
     @hive_restful_response
     def move_file(self, src_path, dst_path):
@@ -126,9 +126,9 @@ class Files:
         return self._move_file(src_path, dst_path, True)
 
     def _move_file(self, src_path, dst_path, is_copy=False):
-        did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
-        full_src_path = self._get_file_full_path(did, app_did, src_path)
-        full_dst_path = self._get_file_full_path(did, app_did, dst_path)
+        user_did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
+        full_src_path = self._get_file_full_path(user_did, app_did, src_path)
+        full_dst_path = self._get_file_full_path(user_did, app_did, dst_path)
         if not full_src_path.exists():
             raise FileNotFoundException(msg='Source file does not exist.')
         if full_dst_path.exists():
@@ -139,19 +139,19 @@ class Files:
             if full_src_path.is_file():
                 shutil.copy2(full_src_path.as_posix(), full_dst_path.as_posix())
                 file_size = os.path.getsize(full_dst_path.as_posix())
-                inc_vault_file_use_storage_byte(did, file_size)
+                inc_vault_file_use_storage_byte(user_did, file_size)
             else:
                 shutil.copytree(full_src_path.as_posix(), full_dst_path.as_posix())
                 dir_size = get_dir_size(full_dst_path.as_posix(), 0.0)
-                inc_vault_file_use_storage_byte(did, dir_size)
+                inc_vault_file_use_storage_byte(user_did, dir_size)
         return {
             'name': dst_path
         }
 
     @hive_restful_response
     def list_folder(self, path):
-        did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
-        full_path = self._get_file_full_path(did, app_did, path)
+        user_did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
+        full_path = self._get_file_full_path(user_did, app_did, path)
         if not full_path.exists() or not full_path.is_dir():
             raise FileNotFoundException(msg='Folder does not exist.')
         files = os.listdir(full_path.as_posix())
@@ -168,8 +168,8 @@ class Files:
             info['size'] = (full_dir_path / file_meta).stat().st_size
         return info
 
-    def get_file_stat_by_did(self, did, app_did, path):
-        full_path = self._get_file_full_path(did, app_did, path)
+    def get_file_stat_by_did(self, user_did, app_did, path):
+        full_path = self._get_file_full_path(user_did, app_did, path)
         if not full_path.exists():
             raise FileNotFoundException(msg='File path does not exist.')
         return full_path, full_path.stat()
@@ -179,8 +179,8 @@ class Files:
         if not path:
             raise InvalidParameterException()
 
-        did, app_did = check_auth_and_vault(VAULT_ACCESS_R)
-        full_path, stat = self.get_file_stat_by_did(did, app_did, path)
+        user_did, app_did = check_auth_and_vault(VAULT_ACCESS_R)
+        full_path, stat = self.get_file_stat_by_did(user_did, app_did, path)
         return {
             'name': path,
             'is_file': full_path.is_file(),
@@ -194,8 +194,8 @@ class Files:
         if not path:
             raise InvalidParameterException()
 
-        did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
-        data, err = query_hash(did, app_did, path)
+        user_did, app_did = check_auth_and_vault(VAULT_ACCESS_WR)
+        data, err = query_hash(user_did, app_did, path)
         if err:
             raise FileNotFoundException(f'Failed getting file hash code: {str(err)}')
         return {

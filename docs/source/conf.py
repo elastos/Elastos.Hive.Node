bbeee70e433897b56a4c76cd2c0e00816a31c058
Fred Chen <chentj070921@yeah.net>
2021-10-14 15:01:28 +0800

    CU-1np1knv - Refine docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,12 +63,16 @@ exclude_patterns = []
 #
 # html_theme = 'alabaster'
 # html_theme = "classic"
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 
-# for theme "sphinx_rtd_theme"
+# for theme 'sphinx_rtd_theme', did not show the content of *.rst file.
 html_show_sourcelink = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+def setup(app):
+    app.add_css_file('css/custom.css')

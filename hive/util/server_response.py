22587ecbafc7f2e5bb128c562e22a0b7b4434cea
Fred Chen <chentj070921@yeah.net>
2022-01-04 16:55:07 +0800

    CU-214eg31 - Send v1 API error response to sentry.

--- hive/util/server_response.py
+++ hive/util/server_response.py
@@ -3,6 +3,8 @@ import json
 from flask import jsonify
 import logging
 
+from sentry_sdk import capture_exception
+
 STATUS = "_status"
 STATUS_OK = "OK"
 STATUS_ERR = "ERR"
@@ -18,6 +20,7 @@ class ServerResponse:
             try:
                 ret.update(data_dic)
             except Exception as e:
+                capture_exception(error=Exception(f'invalid response body: {str(data_dic)}, {str(e)}'))
                 return self.response_err(400, f'invalid response body: {str(data_dic)}, {str(e)}')
         self.logger.debug(json.dumps(ret))
         return jsonify(ret)
@@ -26,4 +29,5 @@ class ServerResponse:
         ret = {STATUS: STATUS_ERR}
         ret.update({"_error": {"code": code, "message": msg}})
         self.logger.error(msg)
+        capture_exception(error=Exception(f'error response: {str(ret)}'))
         return jsonify(ret), code

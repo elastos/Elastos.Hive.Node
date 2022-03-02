cde231e25f35c0563b3a7894af8d23a64675ce13
Fred Chen <chentj070921@yeah.net>
2022-03-02 11:20:02 +0800

    CU-21pbxpy - Refine.

--- src/view/hivehub.py
+++ src/view/hivehub.py
@@ -22,7 +22,8 @@ def init_app(app):
 
 @blueprint.route('/api/hivehub/nodes', methods=['GET'])
 def get_nodes():
-    owner_did = 'did:elastos:ikkFHgoUHrVDTU8HTYDAWH9Z8S377Qvt7n'
+    hive1_url = 'https://hive1.trinity-tech.io:443'
+    owner_did = 'did:elastos:ikkFHgoUHrVDTU8HTYDAWH9Z8S377Qvt7o'
     nodes = [{
         "nid": "jlaksjdflkjasdlkfj001",
         "name": "hive node 节点A",
@@ -31,7 +32,7 @@ def get_nodes():
         "owner_did": owner_did,
         "area": "加拿大 安大略省 多伦多市",
         "email": "1234456789@gamil.com",
-        "url": "http://localhost:5004",
+        "url": hive1_url,
         "remark": "区块链是一个信息技术领域的术语。从本质上讲，它是一个共享数据库，存储于其中的数据或信息，具有“不可伪造”“全程留痕”“可以追溯”“公开透明”“集体维护”等特征。基于这些特征，区块链技术奠定了坚实的“信任”基础，创造了可靠的“合作”机制，具有广阔的运用前景。"
     }, {
         "nid": "jlaksjdflkjasdlkfj002",
@@ -41,7 +42,7 @@ def get_nodes():
         "owner_did": "did:elastos:srgsve5h5yvnwi5yh4hyg2945hvwq0tq",
         "area": "加拿大 安大略省 多伦多市",
         "email": "1234456789@gamil.com",
-        "url": "http://localhost:5004",
+        "url": hive1_url,
         "remark": "区块链是一个信息技术领域的术语。从本质上讲，它是一个共享数据库，存储于其中的数据或信息，具有“不可伪造”“全程留痕”“可以追溯”“公开透明”“集体维护”等特征。基于这些特征，区块链技术奠定了坚实的“信任”基础，创造了可靠的“合作”机制，具有广阔的运用前景。"
     }, {
         "nid": "jlaksjdflkjasdlkfj003",

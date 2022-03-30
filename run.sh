5ea0ea684c5c06b0e96e60435d4dc0fb84327467
Fred Chen <chentj070921@yeah.net>
2022-03-30 16:50:27 +0800

    CU-2hrhh7g - Move out the backup & restore test cases from travis because of the version of mongodump.

--- run.sh
+++ run.sh
@@ -180,6 +180,7 @@ function test () {
     pytest --disable-pytest-warnings -xs tests/scripting_test.py
     pytest --disable-pytest-warnings -xs tests/payment_test.py
     pytest --disable-pytest-warnings -xs tests/backup_test.py
+    pytest --disable-pytest-warnings -xs tests/backup_local_test.py
     pytest --disable-pytest-warnings -xs tests/provider_test.py
 
     pkill -f manage.py

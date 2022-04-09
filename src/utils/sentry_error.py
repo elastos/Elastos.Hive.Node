bea84a4c1d87cf163097b0669ff6a0ed9521a5aa
Fred Chen <chentj070921@yeah.net>
2022-04-09 14:27:05 +0800

    CU-25de4vk - Refine sentry sdk initialize.

--- src/utils/sentry_error.py
+++ src/utils/sentry_error.py
@@ -1,28 +1,19 @@
-import sys
+import logging
 
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
-
-from src.utils.http_exception import HiveException
-
-
-def _make_sentry_excepthook(old_excepthook):
-    """ Comes from exceptionhook.py of sentry_sdk. """
-    # type: (Excepthook) -> Excepthook
-    def sentry_sdk_excepthook(type_, value, traceback):
-        # type: (Type[BaseException], BaseException, TracebackType) -> None
-        if value and isinstance(value, HiveException) or isinstance(value, HiveException):
-            # INFO: Skip the exception from normally throwing because of already back to SDK side.
-            return
-
-        return old_excepthook(type_, value, traceback)
-
-    return sentry_sdk_excepthook
+from sentry_sdk.integrations.logging import LoggingIntegration
 
 
 def init_sentry_hook(sentry_dsn):
     """
     Hook the excepthook to send necessary errors to sentry.
     """
-    sentry_sdk.init(dsn=sentry_dsn, integrations=[FlaskIntegration()], traces_sample_rate=1.0)
-    sys.excepthook = _make_sentry_excepthook(sys.excepthook)
+    # INFO: add logging integration to disable.
+    logging_integration = LoggingIntegration(
+        level=logging.INFO,  # Capture info and above as breadcrumbs
+        event_level=None  # Send errors as events, None to disable
+    )
+    sentry_sdk.init(dsn=sentry_dsn, 
+                    integrations=[FlaskIntegration(), logging_integration],
+                    traces_sample_rate=1.0)

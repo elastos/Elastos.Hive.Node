f4c456b5892471dc6d416fdff7c1f03a0fc0969c
Fred Chen <chentj070921@yeah.net>
2022-05-20 14:28:31 +0800

    CU-2y7tef8 - Add more test cases for scripting service.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 import os
 
+from bson import ObjectId
+from bson.errors import InvalidId
+
 from src.utils.did.did_init import init_did_backend
 
 
 def init_test():
     init_did_backend()
+
+
+def is_valid_object_id(oid: str):
+    try:
+        ObjectId(oid)
+        return True
+    except (InvalidId, TypeError):
+        return False
 
 
 def test_log(*args, **kwargs):

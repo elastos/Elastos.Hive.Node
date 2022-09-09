0ded3d54a6afd638c679b0466547ecca7dc19074
Fred Chen <chentj070921@yeah.net>
2022-09-09 12:55:39 +0800

    CU-3c0c5x9 - Implement encryption with curve25519.

--- src/modules/backup/encryption.py
+++ src/modules/backup/encryption.py
@@ -4,8 +4,14 @@ import base58
 import nacl.secret
 import nacl.utils
 
+from src import hive_setting
+from src.modules.auth import auth
+from src.utils.did.did_wrapper import CipherDecryptionStream
+
 
 class Encryption:
+    TRUNK_SIZE = 4096
+
     def __init__(self, pk: str = None, nonce: str = None):
         self.private_key = base58.b58decode(pk).decode('utf8') \
             if pk is not None else nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
@@ -36,9 +42,40 @@ class Encryption:
         return dst_full_path
 
     @staticmethod
-    def encrypt_file_with_curve25519(src_full_path: Path, other_side_public_key: str) -> Path:
-        pass
+    def __get_cipher(other_side_public_key: str, is_server):
+        auth_ = auth.Auth()
+        doc = auth_.doc
+        cipher = doc.create_curve25519_cipher(auth_.did_str, 3, hive_setting.PASSWORD, is_server)
+        cipher.set_other_side_public_key(other_side_public_key)
+        return cipher
 
     @staticmethod
-    def decrypt_file_with_curve25519(src_full_path: Path, other_side_public_key: str) -> Path:
-        pass
+    def encrypt_file_with_curve25519(src_full_path: Path, other_side_public_key: str, is_server: bool) -> Path:
+        dst_full_path = src_full_path / '.encryption.curve25519'
+        stream = Encryption.__get_cipher(other_side_public_key, is_server).create_encryption_stream()
+        total_size = src_full_path.stat().st_size
+        remain = total_size
+        with open(src_full_path, 'rb') as sf:
+            with open(dst_full_path, 'wb') as bf:
+                while remain > 0:
+                    data = sf.read(Encryption.TRUNK_SIZE)
+                    is_final = remain - len(data) <= 0
+                    cipher_data = stream.push(data, is_final)
+                    bf.write(cipher_data)
+                    remain -= len(data)
+        return dst_full_path
+
+    @staticmethod
+    def decrypt_file_with_curve25519(src_full_path: Path, other_side_public_key: str, is_server: bool) -> Path:
+        dst_full_path = src_full_path / '.decryption.curve25519'
+        stream = Encryption.__get_cipher(other_side_public_key, is_server).create_decryption_stream()
+        total_size = src_full_path.stat().st_size
+        remain = total_size
+        with open(src_full_path, 'rb') as sf:
+            with open(dst_full_path, 'wb') as bf:
+                while remain > 0:
+                    data = sf.read(Encryption.TRUNK_SIZE + CipherDecryptionStream.extra_encryption_size())
+                    plain_data = stream.pull(data)
+                    bf.write(plain_data)
+                    remain -= len(data)
+        return dst_full_path

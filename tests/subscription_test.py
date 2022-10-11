8b9cce2e0918f6427553ad0dddcd6fdec1d3901a
Fred Chen <chentj070921@yeah.net>
2022-10-11 14:40:45 +0800

    CU-3p0q0hd - Add a test case for unsubscribing the vault without force.

--- tests/subscription_test.py
+++ tests/subscription_test.py
@@ -7,7 +7,7 @@ Testing file for the scripting module.
 import unittest
 
 from tests.utils.http_client import HttpClient
-from tests import init_test, test_log
+from tests import init_test, test_log, RA, HttpCode
 
 
 class SubscriptionTestCase(unittest.TestCase):
@@ -41,24 +41,57 @@ class SubscriptionTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
 
     def test06_vault_unsubscribe(self):
-        response = self.cli.delete('/subscription/vault')
+        response = self.cli.delete('/subscription/vault?force=true')
         self.assertIn(response.status_code, [204, 404])
 
-    def test07_price_plan(self):
+    def test07_vault_unsubscribe_without_force(self):
+        """ if unsubscribe without force, the data will keep until next subscribing """
+
+        # subscribe
+        response = self.cli.put('/subscription/vault')
+        RA(response).assert_status(HttpCode.OK)
+
+        # upload a file
+        file_name, file_content = 'abc.txt', '1234567890'
+        response = self.cli.put(f'/vault/files/{file_name}', file_content.encode(), is_json=False)
+        RA(response).assert_status(HttpCode.OK)
+
+        # unsubscribe without force
+        response = self.cli.delete('/subscription/vault?force=false')
+        RA(response).assert_status(HttpCode.NO_CONTENT)
+
+        # get the info. of the vault
+        response = self.cli.get('/subscription/vault')
+        RA(response).assert_status(HttpCode.NOT_FOUND)
+
+        # subscribe again
+        response = self.cli.put('/subscription/vault')
+        RA(response).assert_status(HttpCode.OK)
+
+        # download file
+        response = self.cli.get(f'/vault/files/{file_name}')
+        RA(response).assert_status(HttpCode.OK)
+        self.assertEqual(response.text, file_content)
+
+        # unsubscribe with force
+        response = self.cli.delete('/subscription/vault?force=true')
+        RA(response).assert_status(HttpCode.NO_CONTENT)
+
+    def test10_price_plan(self):
         response = self.cli.get('/subscription/pricing_plan?subscription=all&name=Free')
         self.assertEqual(response.status_code, 200)
         self.assertTrue('backupPlans' in response.json())
         self.assertTrue('pricingPlans' in response.json())
 
-    def test08_backup_subscribe(self):
+    def test20_backup_subscribe(self):
         response = self.backup_cli.put('/subscription/backup')
         self.assertIn(response.status_code, [200, 455])
 
-    def test09_backup_get_info(self):
+    def test21_backup_get_info(self):
         response = self.backup_cli.get('/subscription/backup')
         self.assertEqual(response.status_code, 200)
 
-    def test10_backup_unsubscribe(self):
+    def test22_backup_unsubscribe(self):
         response = self.backup_cli.delete('/subscription/backup')
         self.assertIn(response.status_code, [204, 404])
 

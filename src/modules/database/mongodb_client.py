c2bb4bb1466c855a4e30fcd2ffb843c96b17e1d0
Fred Chen <chentj070921@yeah.net>
2023-03-10 13:22:38 +0800

    CU-864e5gcqm - Refine database service collection definition.

--- src/modules/database/mongodb_client.py
+++ src/modules/database/mongodb_client.py
@@ -1,192 +1,19 @@
 import hashlib
 import logging
-import typing
-from datetime import datetime
+from typing import TypeVar
 
-from bson import ObjectId
+from flask import g
 from pymongo import MongoClient
 from pymongo.errors import CollectionInvalid
 
+from src.modules.database.mongodb_collection import MongodbCollection
 from src.utils.consts import DID_INFO_DB_NAME, COL_IPFS_FILES, SCRIPTING_SCRIPT_COLLECTION, SCRIPTING_SCRIPT_TEMP_TX_COLLECTION, COL_COLLECTION_METADATA, \
     COL_ANONYMOUS_FILES
-from src.utils.http_exception import CollectionNotFoundException, AlreadyExistsException, BadRequestException
+from src.utils.http_exception import CollectionNotFoundException, AlreadyExistsException
 from src import hive_setting
 
-_T = typing.TypeVar('_T', dict, list, tuple)
 
-
-class MongodbCollection:
-    """ all collection wrapper and base class for specific collection
-
-    NOTE: Do not directly create the instance from this class.
-
-    """
-
-    def __init__(self, col, is_management=True):
-        # Collection from pymongo
-        self.col = col
-
-        # management means internal collection which do not support extra features
-        self.is_management = is_management
-
-    def insert_one(self, doc, contains_extra=True, **kwargs):
-        if contains_extra:
-            doc['created'] = doc['modified'] = int(datetime.now().timestamp())
-
-        # kwargs are the options
-        options = {k: v for k, v in kwargs.items() if k in ["bypass_document_validation"]}
-
-        result = self.col.insert_one(self.convert_oid(doc), **options)
-        if not result.inserted_id:
-            raise BadRequestException(f'Failed to insert the doc: {str(doc)}.')
-
-        return {
-            "acknowledged": result.acknowledged,
-            "inserted_id": str(result.inserted_id)  # ObjectId -> str
-        }
-
-    def insert_many(self, docs, contains_extra=True, **kwargs):
-        if contains_extra:
-            for doc in docs:
-                doc['created'] = doc['modified'] = int(datetime.now().timestamp())
-
-        # kwargs are the options
-        options = {k: v for k, v in kwargs.items() if k in ["ordered", "bypass_document_validation"]}
-
-        result = self.col.insert_many(self.convert_oid(docs), **options)
-        if len(result.inserted_ids) < len(docs):
-            raise BadRequestException(f'Failed to insert the docs: {str(docs)}.')
-
-        return {
-            "acknowledged": result.acknowledged,
-            "inserted_ids": [str(oid) for oid in result.inserted_ids]  # ObjectId -> str
-        }
-
-    def update_one(self, filter_, update, contains_extra=True, **kwargs):
-        return self.update_many(filter_, update, contains_extra=contains_extra, only_one=True, **kwargs)
-
-    def update_many(self, filter_, update, contains_extra=True, only_one=False, **kwargs):
-        if contains_extra:
-            now_timestamp = int(datetime.now().timestamp())
-
-            # for normal update
-            if "$set" in update:
-                update["$set"]["modified"] = now_timestamp
-            else:
-                update["$set"] = {"modified": now_timestamp}
-
-            # for insert if not exists
-            if kwargs.get('upsert', False):
-                if "$setOnInsert" in update:
-                    update["$setOnInsert"]["created"] = now_timestamp
-                else:
-                    update["$setOnInsert"] = {"created": now_timestamp}
-
-        # kwargs are the options, and filter them
-        options = {k: v for k, v in kwargs.items() if k in ("upsert", "bypass_document_validation")}
-
-        if only_one:
-            result = self.col.update_one(self.convert_oid(filter_) if filter_ else {}, self.convert_oid(update), **options)
-        else:
-            result = self.col.update_many(self.convert_oid(filter_) if filter_ else {}, self.convert_oid(update), **options)
-        return {
-            "acknowledged": result.acknowledged,
-            "matched_count": result.matched_count,
-            "modified_count": result.modified_count,
-            "upserted_id": str(result.upserted_id) if result.upserted_id else None
-        }
-
-    def replace_one(self, filter_, document, upsert=True):
-        # default 'bypass_document_validation': False
-        result = self.col.replace_one(self.convert_oid(filter_) if filter_ else None, self.convert_oid(document), upsert=upsert)
-        return {
-            "acknowledged": result.acknowledged,
-            "matched_count": result.matched_count,
-            "modified_count": result.modified_count,
-            "upserted_id": str(result.upserted_id) if result.upserted_id else None  # ObjectId -> str
-        }
-
-    def find_one(self, filter_: dict, **kwargs) -> dict:
-        """ Note: the result document contains ObjectId or other types
-        which can not directly take as response body. """
-        result = self.find_many(filter_, only_one=True, **kwargs)
-        return result[0] if result else None
-
-    def find_many(self, filter_: dict, only_one=False, **kwargs) -> list:
-        """ Note: the result documents contain ObjectId or other types
-                which can not directly take as response body. """
-
-        # kwargs are the options
-        options = {k: v for k, v in kwargs.items() if k in ("projection",
-                                                            "skip",
-                                                            "limit",
-                                                            "sort",
-                                                            "allow_partial_results",
-                                                            "return_key",
-                                                            "show_record_id",
-                                                            "batch_size")}
-
-        # extra sort support
-        if 'sort' in options:
-            if isinstance(options['sort'], dict):
-                # value example: {'author', -1} => [('author', -1)]
-                options['sort'] = [(k, v) for k, v in options['sort'].items()]
-
-        # BUGBUG: Getting all documents out maybe not well.
-        if only_one:
-            result = self.col.find_one(self.convert_oid(filter_) if filter_ else None, **kwargs)
-            return [] if result is None else [result]
-
-        return list(self.col.find(self.convert_oid(filter_) if filter_ else None, **options))
-
-    def count(self, filter_, **kwargs):
-        options = {k: v for k, v in kwargs.items() if k in ("skip", "limit", "maxTimeMS")}
-
-        return self.col.count_documents(self.convert_oid(filter_) if filter_ else {}, **options)
-
-    def delete_one(self, filter_):
-        return self.delete_many(filter_, only_one=True)
-
-    def delete_many(self, filter_, only_one=False):
-        if only_one:
-            result = self.col.delete_one(self.convert_oid(filter_) if filter_ else None)
-        else:
-            result = self.col.delete_many(self.convert_oid(filter_) if filter_ else None)
-        return {
-            "acknowledged": result.acknowledged,
-            "deleted_count": result.deleted_count
-        }
-
-    def distinct(self, field: str) -> list:
-        return self.col.distinct(field)
-
-    def convert_oid(self, value: _T):
-        """ try to convert the following dict recursively.
-
-            { "group_id": {"$oid": "5f497bb83bd36ab235d82e6a"} }
-
-        to:
-
-            { "group_id": ObjectId("5f497bb83bd36ab235d82e6a") }
-        """
-        # The management collection do not do $oid checking.
-        if self.is_management:
-            return value
-
-        if type(value) in (list, tuple):
-            for o in value:
-                if isinstance(o, dict) or type(o) in (list, tuple):
-                    self.convert_oid(o)
-        elif isinstance(value, dict):
-            for k, v in value.copy().items():
-                if isinstance(v, dict):
-                    if '$oid' in v:
-                        value[k] = ObjectId(v['$oid'])
-                    else:
-                        value[k] = self.convert_oid(v)
-                elif type(v) in (list, tuple):
-                    value[k] = self.convert_oid(v)
-        return value
+_T = TypeVar('_T', bound=MongodbCollection)
 
 
 class MongodbClient:
@@ -249,10 +76,11 @@ class MongodbClient:
         md5.update((user_did + "_" + app_did).encode("utf-8"))
         return prefix + str(md5.hexdigest())
 
-    def get_management_collection(self, col_name) -> MongodbCollection:
+    def get_management_collection(self, col_name: str) -> MongodbCollection:
         """ Get the collection used for node management.
         It will be created if not exists.
 
+        TODO: replace this with get_collection
         """
 
         database = self.__get_database(DID_INFO_DB_NAME)
@@ -260,13 +88,14 @@ class MongodbClient:
             database.create_collection(col_name)
         return MongodbCollection(database[col_name])
 
-    def get_user_collection(self, user_did: str, app_did: str, col_name) -> MongodbCollection:
+    def get_user_collection(self, user_did: str, app_did: str, col_name: str) -> MongodbCollection:
         """ User application collection belongs to user database and will check the existence.
         Internal collection will be created automatically.
 
         :raise: CollectionNotFoundException
-        """
 
+        TODO: replace this with get_collection
+        """
         is_internal = col_name in MongodbClient.INTERNAL_USER_COLLECTIONS
 
         database = self.__get_database(MongodbClient.get_user_database_name(user_did, app_did))
@@ -275,7 +104,33 @@ class MongodbClient:
                 database.create_collection(col_name)
             else:
                 raise CollectionNotFoundException(f'Can not find collection {col_name}')
+
         return MongodbCollection(database[col_name], is_management=False)
+
+    def get_collection(self, user_did: str, app_did: str, col_class: type(_T) = MongodbCollection) -> _T:
+        """ Use for get collection with the specific class. """
+        col_name, is_management = col_class.get_name(), col_class.is_management()
+        assert col_name
+
+        if is_management:
+            database = self.__get_database(DID_INFO_DB_NAME)
+            if col_name not in database.list_collection_names():
+                database.create_collection(col_name)
+        else:
+            database = self.__get_database(MongodbClient.get_user_database_name(user_did, app_did))
+            if col_name not in database.list_collection_names():
+                if col_class.is_internal():
+                    database.create_collection(col_name)
+                else:
+                    raise CollectionNotFoundException(f'Can not find collection {col_name}')
+
+        col = col_class(database[col_name])
+        col.user_did = user_did
+        col.app_did = app_did
+        return col
+
+    def get_col(self, col_class: type(_T) = MongodbCollection) -> _T:
+        return self.get_collection(g.usr_did, g.app_did, col_class)
 
     def get_user_collection_names(self, user_did: str, app_did: str):
         """ Get collection names belongs to the user's application """
@@ -327,3 +182,6 @@ class MongodbClient:
 
         # count size by command: https://www.mongodb.com/docs/v4.4/reference/command/dbStats/
         return int(database.command('dbstats')['totalSize'])
+
+
+mcli = MongodbClient()
